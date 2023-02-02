887d4c3e4c4525163476f88a7511a572fc277e18
YuliaSavchik <yulia.deamchik@gmail.com>
2023-02-02 10:33:38 +0300

    feat: create main page content

--- src/components/mainHeader/index.ts
+++ src/components/mainHeader/index.ts
@@ -1,17 +1,4 @@
-const createHtmlElement = (tegName: string, className: string) => {
-  const element: HTMLElement = document.createElement(tegName);
-  element.classList.add(className);
-
-  return element;
-};
-
-const createButtonElement = (className: string, content: string) => {
-  const btn: HTMLButtonElement = document.createElement('button');
-  btn.classList.add(className);
-  btn.textContent = content;
-
-  return btn;
-};
+import { createHtmlElement, createButtonElement } from '../../utils';
 
 export const createMainHeader = () => {
   const header = createHtmlElement('div', 'header');
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import './main.scss';
 import 'normalize.css';
 
 import { createMainHeader } from './components/mainHeader';
+import { createMainContent } from './components/mainContent';
 
 const header = createMainHeader();
+const main = createMainContent();
 const container = document.querySelector('.content');
-container?.append(header);
+container?.append(header, main);
