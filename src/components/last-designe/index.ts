1d145eb7d44d567db2a9225f32a4dd3dd8e64cc7
YuliaSavchik <yulia.deamchik@gmail.com>
2023-02-23 14:58:05 +0300

    fix: fix adding to last designe

--- src/components/last-designe/index.ts
+++ src/components/last-designe/index.ts
@@ -3,28 +3,32 @@ const postcardLayout0: HTMLElement[] = [];
 const postcardLayout1: HTMLElement[] = [];
 const postcardLayout2: HTMLElement[] = [];
 const postcardLayout3: HTMLElement[] = [];
-collectionPostcard.push(postcardLayout0, postcardLayout1, postcardLayout2, postcardLayout3);
+const postcardLayoutEmpty: HTMLElement[] = [];
+collectionPostcard.push(postcardLayout0, postcardLayout1, postcardLayout2, postcardLayout3, postcardLayoutEmpty);
 
 const collectionLogotype: HTMLElement[][] = [];
 const logotypeLayout0: HTMLElement[] = [];
 const logotypeLayout1: HTMLElement[] = [];
 const logotypeLayout2: HTMLElement[] = [];
 const logotypeLayout3: HTMLElement[] = [];
-collectionPostcard.push(logotypeLayout0, logotypeLayout1, logotypeLayout2, logotypeLayout3);
+const logotypeLayoutEmpty: HTMLElement[] = [];
+collectionPostcard.push(logotypeLayout0, logotypeLayout1, logotypeLayout2, logotypeLayout3, logotypeLayoutEmpty);
 
 const collectionVisitCard: HTMLElement[][] = [];
 const visitcardLayout0: HTMLElement[] = [];
 const visitcardLayout1: HTMLElement[] = [];
 const visitcardLayout2: HTMLElement[] = [];
 const visitcardLayout3: HTMLElement[] = [];
-collectionPostcard.push(visitcardLayout0, visitcardLayout1, visitcardLayout2, visitcardLayout3);
+const visitcardLayoutEmpty: HTMLElement[] = [];
+collectionPostcard.push(visitcardLayout0, visitcardLayout1, visitcardLayout2, visitcardLayout3, visitcardLayoutEmpty);
 
 const collectionResume: HTMLElement[][] = [];
 const resumeLayout0: HTMLElement[] = [];
 const resumeLayout1: HTMLElement[] = [];
 const resumeLayout2: HTMLElement[] = [];
 const resumeLayout3: HTMLElement[] = [];
-collectionPostcard.push(resumeLayout0, resumeLayout1, resumeLayout2, resumeLayout3);
+const resumeLayoutEmpty: HTMLElement[] = [];
+collectionPostcard.push(resumeLayout0, resumeLayout1, resumeLayout2, resumeLayout3, resumeLayoutEmpty);
 
 export const lastDesigneCollection = [collectionPostcard, collectionLogotype, collectionVisitCard, collectionResume];
 
@@ -33,16 +37,21 @@ const saveUsingLayout = (
   layout1: HTMLElement[],
   layout2: HTMLElement[],
   layout3: HTMLElement[],
+  layoutEmpty: HTMLElement[],
 ) => {
 
   const layout = document.querySelector('.container') as HTMLElement;
+  const childrenCount = layout.childElementCount;
+  const bcColor = layout.style.backgroundColor;
   const id = layout.getAttribute('id');
 
   if (id === '0') layout0.push(layout);
   else if (id === '1') layout1.push(layout);
   else if (id === '2') layout2.push(layout);
   else if (id === '3') layout3.push(layout);
-  else if (id === 'empty') return;
+  else if (id === 'empty' && childrenCount > 0 || 
+          id === 'empty' && childrenCount >= 0 && bcColor !== 'white') layoutEmpty.push(layout);
+  else if (id === 'empty' && childrenCount === 0) return;
 
 };
 
@@ -51,39 +60,43 @@ mainContainer?.addEventListener('click', (event) => {
   const item = event.target;
   const clickedItem = item as HTMLElement;
 
-  if (clickedItem.closest('.postcard') && clickedItem.getAttribute('id') !== 'empty') {
+  if (clickedItem.closest('.postcard')) {
     saveUsingLayout(
       postcardLayout0,
       postcardLayout1,
       postcardLayout2,
       postcardLayout3,
+      postcardLayoutEmpty,
     );
   }
 
-  if (clickedItem.closest('.logotype') && clickedItem.getAttribute('id') !== 'empty') {
+  if (clickedItem.closest('.logotype')) {
     saveUsingLayout(
       logotypeLayout0,
       logotypeLayout1,
       logotypeLayout2,
       logotypeLayout3,
+      logotypeLayoutEmpty,
     );
   }
 
-  if (clickedItem.closest('.visit-card') && clickedItem.getAttribute('id') !== 'empty') {
+  if (clickedItem.closest('.visit-card')) {
     saveUsingLayout(
       visitcardLayout0,
       visitcardLayout1,
       visitcardLayout2,
       visitcardLayout3,
+      visitcardLayoutEmpty,
     );
   }
 
-  if (clickedItem.closest('.resume') && clickedItem.getAttribute('id') !== 'empty') {
+  if (clickedItem.closest('.resume')) {
     saveUsingLayout(
       resumeLayout0,
       resumeLayout1,
       resumeLayout2,
       resumeLayout3,
+      resumeLayoutEmpty,
     );
   }
 });
